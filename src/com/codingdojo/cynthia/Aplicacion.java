53a47470cdbd265cf1cccdbb89cfef59e67a9594
Cynthia Castillo <ca.castillo@gmail.com>
2022-03-31 17:26:35 -0600

    Arboles

---
+++ src/com/codingdojo/cynthia/Aplicacion.java
@@ -0,0 +1,16 @@
+package com.codingdojo.cynthia;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		
+		Arbol arbol = new Arbol();
+		
+		arbol.insertarPalabra("MIO");
+		arbol.insertarPalabra("MIA");
+		arbol.insertarPalabra("MIU");
+		arbol.insertarPalabra("VE");
+
+	}
+
+}
